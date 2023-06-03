8e3ca63b344523c86a6bac3c2ab0f0fece5d37bd
adrianblazejczyk <adrian.blazejczyk@icloud.com>
2023-06-03 12:12:08 +0200

    second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,25 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+// Change code below this line
+const gallery = document.querySelector(".gallery");
+
+function createGallery(listImage) {
+  const gallery = listImage
+    .map(
+      (image) =>
+     `<li><div><a class="gallery__item" href="${image.original}"><img class="gallery__image" src="${image.preview}" alt="${image.description}" /></a></div></li>`
+      //  `<li> <div><a href=""><img class="gallery__image"  src=${image.preview} data-source=${image.original} alt="${image.description}"></a></div> </li>`
+      //console.log(image.original)
+    )
+    .join("");
+
+  return gallery;
+}
+function addElement(callback, listElement, querySelector) {
+  querySelector.insertAdjacentHTML("beforeend", callback(listElement));
+}
+addElement(createGallery, galleryItems, gallery);
+const lightbox = new SimpleLightbox('.gallery a', {captionsData:"alt", captionDelay: 250 });
+
+//console.log(galleryItems);
+//const gallery1 = document.querySelector(".gallery");
